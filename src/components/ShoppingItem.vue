0eac5d6281081a94415352e2154da0be7167daf8
Sergio Juarez <bleding.death@gmail.com>
2020-04-23 14:55:59 -0500

    Adds ShoppingItem and ShoppingList components

---
+++ src/components/ShoppingItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form>
+      <label>Item name</label>
+      <input type="text" />
+      <br />
+      <label>Quantity</label>
+      <input v-model="quantity" />
+      <br />
+      <label>Unit Price</label>
+      <input v-model="unitPrice" type="text" />
+      <br />
+      <label>Total Price</label>
+      <input :value="quantity * unitPrice" readonly disabled />
+    </form>
+    <br/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingItem",
+  data() {
+    return {
+      quantity: null,
+      unitPrice: null
+    };
+  },
+  props: ["id"]
+};
+</script>
---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form>
+      <label>Shoping Date</label>
+      <input v-model="shoppingDate" type="date"/>
+      <br />
+      <label>Store</label>
+      <input v-model="store" />
+      <br />
+    </form>
+    <br/>
+    <ShoppingItem
+      v-for="item in shoppingItems"
+      v-bind:key="item.id"
+      v-bind:id="item.id"
+    />
+    <button v-on:click="addItem()">Add Item</button>
+  </div>
+</template>
+
+<script>
+import ShoppingItem from "./ShoppingItem.vue";
+
+export default {
+  name: "ShoppingList",
+  data() {
+    return {
+      shoppingDate: null,
+      store: null,
+      shoppingItems: []
+    };
+  },
+  methods: {
+    addItem() {
+      this.shoppingItems.push({ id: this.shoppingItems.length + 1 });
+    }
+  },
+  components: {
+    ShoppingItem
+  }
+};
+</script>
