f922386ac915ce9ff5ecb2b8f924d0a32e90430f
Sergio Juarez <bleeding.death@gmail.com>
2020-07-06 19:03:19 -0500

    Adds production endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 export const baseUrl = "http://localhost:9000/api";
+// export const baseUrl = "http://3.12.107.15:9000/api";
 export const defaultUser = "user";
 const defaultPassword = "summer2020";
 
