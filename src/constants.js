71ae120c690f44bef981aa443cb69f2ef884a161
Sergio Juarez <bleeding.death@gmail.com>
2020-07-13 23:25:29 -0500

    Sets environment variables

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,5 @@
 export const baseUrl = "http://localhost:9000/api";
-// export const baseUrl = "http://3.12.107.15:9000/api";
-export const defaultUser = "user";
-const defaultPassword = "summer2020";
-
 export default {
-    username: defaultUser,
-    password: defaultPassword
+    username: process.env.VUE_APP_DEFAULT_USERNAME,
+    password: process.env.VUE_APP_DEFAULT_PASSWORD
 }
