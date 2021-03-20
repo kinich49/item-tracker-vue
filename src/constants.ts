c2c14612656c141da1c445d62d6db99880642649
Sergio Juarez <bleeding.death@gmail.com>
2021-03-20 01:14:45 -0600

    Fixes port

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8081/api";
+export const baseUrl = "http://localhost:9000/api";
 export default {
     username: process.env.VUE_APP_DEFAULT_USERNAME,
     password: process.env.VUE_APP_DEFAULT_PASSWORD
